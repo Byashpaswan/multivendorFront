74b81d11afc78ab1a8bf394a62e5ff9491505c3a
ByashPaswan <byas.paswan99@gmail.com>
2025-09-07 23:07:23 +0530

    update

--- src/util/uploadToCloudnary.ts
+++ src/util/uploadToCloudnary.ts
@@ -1,26 +1,61 @@
-export const uploadToCloudinary = async (pics:any) => {
+// export const uploadToCloudinary = async (pics:any) => {
+//    console.log("pics : ", pics);
 
-    const cloud_name="dex3llsph"
-    const upload_preset="my"
+//     const cloud_name="dex3llsph"
+//     const upload_preset="my"
     
-    if (pics) {
+//     if (pics) {
       
-      const data = new FormData();
-      data.append("file", pics);
-      data.append("upload_preset", upload_preset);
-      data.append("cloud_name", cloud_name);
+//       const data = new FormData();
+//       data.append("file", pics);
+//       data.append("upload_preset", upload_preset);
+//       data.append("cloud_name", cloud_name);
   
-      const res = await 
-      fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
-        method: "post",
-        body: data,
-      })
+//       const res = await 
+//       fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
+//         method: "post",
+//         body: data,
+//       })
         
-        const fileData=await res.json();
-        console.log("url : ", fileData);
-        return fileData.url
+//       console.log("res : ", res);
+//         const fileData=await res.json();
+//         console.log("url : ", fileData);
+//         return fileData.url
   
-    } else {
-      console.log("error");
-    }
-  };
+//     } else {
+//       console.log("error");
+//     }
+//   };
+
+
+export const uploadToCloudinary = async (pics: File) => {
+  const cloud_name = "dex3llsph";
+  const upload_preset = "capitalMart";
+
+  if (!pics) {
+    console.log("error: no file provided");
+    return null;
+  }
+
+  const data = new FormData();
+  data.append("file", pics);
+  data.append("upload_preset", upload_preset);
+
+  try {
+    const res = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+
+    const fileData = await res.json();
+    console.log("uploaded file:", fileData);
+
+    return fileData.secure_url; // use secure_url instead of url
+  } catch (err) {
+    console.error("Upload error:", err);
+    return null;
+  }
+};
